c588c94201733b56527aecd64f93c7608e520aed
Juan Macias <156457031+jmp89@users.noreply.github.com>
2024-03-23 16:28:30 +0000

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*  RECURSOS
-API: https://gist.github.com/bertez/2528edb2ab7857dae29c39d1fb669d31
-- Lista de preguntas
-- document.createElement
-- Element.innerHTML
- https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
- https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
-*/
